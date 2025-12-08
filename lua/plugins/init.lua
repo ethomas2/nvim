74daeb8e077721363f5b4b86fe6d5524a4057c91
Evan Thomas <evanrthom@gmail.com>
2025-12-08 14:25:35 -0800

    Remove gitsigns config update
    Idk what this was doing, but it was thowing an error

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -88,10 +88,6 @@ local default_plugins = {
     opts = function()
       return require("plugins.configs.others").gitsigns
     end,
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "git")
-      require("gitsigns").setup(opts)
-    end,
   },
 
   -- lsp stuff
